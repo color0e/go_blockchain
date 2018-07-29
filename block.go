869bb72ddae2bfa1191e9b192ba9483970098706
DESKTOP-JLS7ABK\User <limjangsoon@naver.com>
2018-07-29 23:17:59 +0900

    part3. CLI and persistence

--- block.go
+++ block.go
@@ -3,6 +3,9 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+	"log"
 	"strconv"
 	"time"
 )
@@ -20,6 +23,28 @@ func (b *Block) SetHash() {
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
 	hash := sha256.Sum256(headers)
 	b.Hash = hash[:]
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-
 	bc := NewBlockchain()
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 1 more to Ivan")
+	defer bc.db.Close()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Timestamp: %s\n", time.Unix(block.Timestamp, 0))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
